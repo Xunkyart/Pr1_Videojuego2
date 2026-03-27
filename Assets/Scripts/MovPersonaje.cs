814925feb59b7855eec7609409e7e5b17d24a7df
Xunkyart <xunky.art@gmail.com>
2026-03-27 12:39:44 +0100

    Podemos mover al personaje en x e y con imputs

--- Assets/Scripts/MovPersonaje.cs
+++ Assets/Scripts/MovPersonaje.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
 
+using UnityEngine.InputSystem;
+
 public class MovPersonaje : MonoBehaviour
 {
+
+ public float velocidad = 0.05f;
 
     public Vector3 inicioPersonaje = new Vector3(-3.5f, 0, 0);
 
@@ -15,16 +19,30 @@ public class MovPersonaje : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+      Vector2 moveInput =  (InputSystem.actions["Move"].ReadValue<Vector2>());
+
+       this.transform.Translate(moveInput.x*velocidad, moveInput.y*velocidad, 0);
+
+     
+
+
+      
+
+
+
+
+
+
+
+
 /*
       Esto mueve solo al personaje 0.01 en x cada frame. Ambas límeas de código hacen lo mismo.
 
       this.transform.position = new Vector3(this.transform.position.x + 0.01f, this.transform.position.y,0);
 
-      this.transform.Translate(0.01f,0,0);
+     
 */
-
-
-      
 
     
     }
