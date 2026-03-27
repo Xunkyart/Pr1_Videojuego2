899133aeef9af6afb7833b213837cd49c069b186
Xunkyart <xunky.art@gmail.com>
2026-03-27 12:09:07 +0100

    El personaje se mueve solo

--- Assets/Scripts/MovPersonaje.cs
+++ Assets/Scripts/MovPersonaje.cs
@@ -3,66 +3,34 @@ using UnityEngine;
 public class MovPersonaje : MonoBehaviour
 {
 
-    int miNumero = 3;
-
-    float miNumeroFlotante = 0.8f;
-
-    string miCadenaDeTexto = "Hola, esto es una cadena de texto";
-
-    bool miBoolean = true;
-
-
+    public Vector3 inicioPersonaje = new Vector3(-3.5f, 0, 0);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        float sumaEntreDecenas = Sumar(10,20,3.8f);
 
-        Debug.Log("inicio");
-        Debug.Log(sumaEntreDecenas);
-
+      this.transform.position = inicioPersonaje;
+      
     }
-
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("hola");
-    }
+/*
+      Esto mueve solo al personaje 0.01 en x cada frame. Ambas límeas de código hacen lo mismo.
+
+      this.transform.position = new Vector3(this.transform.position.x + 0.01f, this.transform.position.y,0);
+
+      this.transform.Translate(0.01f,0,0);
+*/
 
 
-    float Sumar (int num1, int num2, float num3)
-    {
+      
 
-        float suma = num1 + num2 + num3;
-
-        return suma;
     
     }
 
 
-/*
-&& a y b obligatorias
-|| a o b, una de las dos
-|| numa + numb = true then pasan cosas
 
-Contador 
-for (int i = 0; i <=10; i++)
-{
-    Debug.log(i+"veces");
-}
-
-
-
-
-*/
-
-    //Esto es un comentario. uwu. 
-
-    /*
-    jsahbsadbdsaj
-    akjshdkjashd
-    ajshgdasd 
-    */
     
 
 
