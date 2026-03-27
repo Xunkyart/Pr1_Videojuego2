85e57f9b6b9cbcacef0f14d025f5f15abc7f6644
Xunkyart <xunky.art@gmail.com>
2026-03-27 13:40:18 +0100

    nos movemos y cambia el eje del sprite

--- Assets/Scripts/MovPersonaje.cs
+++ Assets/Scripts/MovPersonaje.cs
@@ -20,9 +20,16 @@ public class MovPersonaje : MonoBehaviour
     void Update()
     {
 
-      Vector2 moveInput =  (InputSystem.actions["Move"].ReadValue<Vector2>());
+      Vector2 moveInput =  (InputSystem.actions["Move"].ReadValue<Vector2>()); //Esto nos da [-1,1] , [0,1], ... lo que corresponda. Aquí resumimos todo el codigo de la línea en moveInput para no repetirlo todo
 
        this.transform.Translate(moveInput.x*velocidad, moveInput.y*velocidad, 0);
+       
+       if (moveInput.x < 0)
+       {
+        this.GetComponent<SpriteRenderer>().flipX = true;
+       }else if (moveInput.x>0){
+          this.GetComponent<SpriteRenderer>().flipX = false;
+        }
 
      
 
